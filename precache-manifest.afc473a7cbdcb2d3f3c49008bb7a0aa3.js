62a80967d734275469d6f91daa0c672e2f22c364
Filip Kornaus <kornaus.filip@gmail.com>
2020-05-13 14:32:22 +0200

    Updates

---
+++ precache-manifest.afc473a7cbdcb2d3f3c49008bb7a0aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1854e52b8e410fb276971e657ffebea",
+    "url": "/Pokemon_Cards/index.html"
+  },
+  {
+    "revision": "748af62b2ee1dfee0475",
+    "url": "/Pokemon_Cards/static/js/2.897b7650.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokemon_Cards/static/js/2.897b7650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a51a36123b9a0a130d96",
+    "url": "/Pokemon_Cards/static/js/main.4e7219fd.chunk.js"
+  },
+  {
+    "revision": "9db95a99656515d6dfba",
+    "url": "/Pokemon_Cards/static/js/runtime-main.bf6c833c.js"
+  }
+]);
